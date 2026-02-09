0136c3baa9970179374735d7d3b381e04309dd57
Zhirnov Ilay <zirnovila34@gmail.com>
2026-02-09 17:06:48 +0300

    bag fixes

--- telegram_delivery.py
+++ telegram_delivery.py
@@ -16,7 +16,6 @@ class TelegramDelivery:
         content = self._escape_html(news_item['content'])
         rubric = self._escape_html(news_item['rubric'])
         link = self._escape_html(news_item['link'])
-        print(f"link: {link}")
 
         rubric_emojis = {
             'Anniversary time': '🎉',
